9296908ef37c7e6e89e8f0ad2eb7373b0557d848
Sreymey <sreymey.nai@institute.pse.ngo>
2025-04-28 10:24:54 +0700

    my code ceckin checjout

--- src/interfaces/inviteInterface.ts
+++ src/interfaces/inviteInterface.ts
@@ -6,14 +6,17 @@ export interface IInvitee {
     qr_code: string;
     is_checked_in: boolean; 
     checked_in_at: Date | null; 
+    gift: string | null; // <-- added this line
     created_at?: Date;
 }
+
 export interface IInviteeWithoutId extends Omit<IInvitee, 'id' | 'created_at'> {
     event_id: string; 
     status: 'pending' | 'accept' | 'maybe' | 'no' | 'busy'; 
     qr_code: string;
     is_checked_in: boolean; 
     checked_in_at: Date | null;
+    gift: string | null; // <-- added this line
 }
 
 
@@ -27,10 +30,13 @@ export interface IInviteeRepository {
     delete(id: string): Promise<void>;
     findInviteeByEventId(event_id: string): Promise<IInvitee[]>;
     countInviteeStatusByEventId(event_id: string): Promise<{ status: string, count: number }[]>;
-    
+   checkin(event_id:string,user_id:string):Promise<IInvitee>;
+   checkout(event_id: string, user_id: string, gift: string): Promise<IInvitee>;
 }
 
 export interface IInviteeService extends IInviteeRepository {
     countInviteeStatusByEventId(event_id: string):  Promise<{ status: string, count: number }[]>;
     findInviteeByEventId(event_id: string): Promise<IInvitee[]>;
+    checkin(event_id:string,user_id:string):Promise<IInvitee> ;
+    checkout(event_id: string, user_id: string, gift: string): Promise<IInvitee>;
 }
